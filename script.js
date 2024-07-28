34d41da90d8551b3a8521c0124b31b6ecff3435a
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2024-07-28 21:42:23 +0545

    Create fully funtional non-decimal number calculator

--- script.js
+++ script.js
@@ -23,38 +23,70 @@ function operate(num1, num2, operation) {
     if (operation == '÷') return divide(num1, num2);
 }
 
+
 const buttons = document.querySelectorAll('.buttons .number');
 const display = document.querySelector('.display');
 const operation = document.querySelectorAll('.operator');
 const equals = document.querySelector('.equals-to');
 const clear = document.querySelector('.all-clear');
 
+function resetButtonOpacity() {
+    for (let i = 0; i < operation.length; i++) {
+        operation[i].style.opacity = '1';
+    }
+}
+
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function () {
-        if(display.textContent == '0'){
+        resetButtonOpacity();
+        if (display.textContent == '0' && buttons[i].textContent != '.') {
             display.textContent = '';
         }
-        display.textContent += buttons[i].textContent;
-    })
-}
-
-for(let i = 0; i<operation.length;i++) {
-    operation[i].addEventListener('click', function(){
-        firstNumber = +display.textContent;
-        operator = operation[i].textContent;
-        display.textContent = '';
-    })
-}
-
-equals.addEventListener('click', function(){
-    if(firstNumber){
-        if(operator){
-            display.textContent = operate(firstNumber, +display.textContent, operator);
+        else if (display.textContent == '0' && buttons[i].textContent == '.') {
+            display.textContent = '0';
         }
-    }
-})
+        if (firstNumber && operator) {
+            if (!secondNumber) {
+                display.textContent = '';
+            }
+            display.textContent += buttons[i].textContent;
+            secondNumber = +display.textContent;
+        }
+        else {
+            display.textContent += buttons[i].textContent;
+        }
+    })
+}
 
-clear.addEventListener('click', function(){
+for (let i = 0; i < operation.length; i++) {
+    operation[i].addEventListener('click', function () {
+        operation[i].style.opacity = 0.5;
+        if (!secondNumber) {
+            firstNumber = +display.textContent;
+            operator = operation[i].textContent;
+        }
+        if (secondNumber) {
+            firstNumber = Number.isInteger(operate(firstNumber, secondNumber, operator)) ? operate(firstNumber, secondNumber, operator) : operate(firstNumber, secondNumber, operator).toFixed(3);
+            operator = operation[i].textContent;
+            display.textContent = firstNumber;
+            secondNumber = undefined;
+        }
+    })
+}
+
+
+equals.addEventListener('click', function () {
+    if (firstNumber && operator) {
+        display.textContent = Number.isInteger(operate(firstNumber, secondNumber, operator)) ? operate(firstNumber, secondNumber, operator) : operate(firstNumber, secondNumber, operator).toFixed(3);
+        firstNumber = +display.textContent;
+        secondNumber = undefined;
+        operator = undefined;
+    }
+}
+)
+
+clear.addEventListener('click', function () {
+    resetButtonOpacity();
     display.textContent = '0';
     firstNumber = undefined;
     secondNumber = undefined;
